fe1b492c90d91701731c60a1930243135274d009
Namy07 <mryadav94@gmail.com>
2016-11-01 01:05:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/abdul-kalam.html',function(req,res)  {
+app.get('/abdul-kalam',function(req,res)  {
    res.sendFile(path.join(__dirname, 'ui', 'abdul-kalam.html'));
 });
 
