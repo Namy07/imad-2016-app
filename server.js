ca36c7b3ce68bb088792c782975dbfce7d26e19f
Namy07 <mryadav94@gmail.com>
2016-11-01 00:00:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+app.get('/abdulkalam.html',function(req,res)  {
+   res.sendFile(path.join(__dirname, 'ui', 'abdulkalam.html'));
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
